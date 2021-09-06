5c4a87b07c605111b1a31531c01c7586f8ccce91
Taylor <taylor@galviadigital.com>
2021-09-06 09:52:23 +0100

    ES6 syntax

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,8 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express';
+const indexRouter = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  return res.status(200).json({ message: 'Welcome to Express API template' });
 });
 
 module.exports = router;
