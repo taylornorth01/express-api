72d50615e08dd4eb8fd457f7a3efeec05af6048c
Taylor <taylor@galviadigital.com>
2021-09-06 11:23:42 +0100

    Initializing sequalize

--- src/app.js
+++ src/app.js
@@ -2,6 +2,28 @@ import logger from 'morgan';
 import express from 'express';
 import cookieParser from 'cookie-parser';
 import indexRouter from './routes/index';
+
+/*
+ *  Setting up Sequalize with Postgres
+ */
+
+import { DB_POSTGRES } from './settings';
+import Sequelize from 'sequelize';
+const sequelize = new Sequelize(DB_POSTGRES);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+/*
+ *  Setting up the Express app
+ */
+
 const app = express();
 
 app.use(logger('dev'));
