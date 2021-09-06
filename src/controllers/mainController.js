8e37729ec2e9bcf240742623800996ebd9d4c5b0
Taylor <taylor@galviadigital.com>
2021-09-06 11:24:51 +0100

    Changed index message

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,4 +1,3 @@
-import { testEnvironmentVariable } from '../settings';
-
-export const indexPage = (req, res) =>
-  res.status(200).json({ message: testEnvironmentVariable });
+export const indexPage = async (req, res) => {
+  res.status(200).json({ message: 'Welcome to your express application' });
+};
