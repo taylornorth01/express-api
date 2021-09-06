7542c378d26b2d52fbf6e0200163d3b2cd9f14e5
Taylor <taylor@galviadigital.com>
2021-09-06 12:37:57 +0100

    Removed redundant code

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,11 +1,3 @@
-import { Note } from '../app';
-
 export const indexPage = (req, res) => {
   res.status(200).json({ message: 'Welcome to your express application' });
-};
-
-export const notesPage = (req, res) => {
-  Note.findAll().then((notes) => {
-    res.json(notes);
-  });
 };
