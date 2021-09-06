1d2e423b3ea3c282bac6c4fcbb7041ae95898e69
Taylor <taylor@galviadigital.com>
2021-09-06 11:36:38 +0100

    Notes page and /notes route

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,3 +1,11 @@
-export const indexPage = async (req, res) => {
+import { Note } from '../app';
+
+export const indexPage = (req, res) => {
   res.status(200).json({ message: 'Welcome to your express application' });
+};
+
+export const notesPage = (req, res) => {
+  Note.findAll().then((notes) => {
+    res.json(notes);
+  });
 };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { indexPage } from '../controllers';
+import { indexPage, notesPage } from '../controllers';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexPage);
+indexRouter.get('/notes', notesPage);
 
 export default indexRouter;
