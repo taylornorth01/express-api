72d40c3a32dc47d0dddfb6d5fe8b9e1ed0bd17c8
Taylor <taylor@galviadigital.com>
2021-09-06 11:36:18 +0100

    Notes example sequalize model

--- src/app.js
+++ src/app.js
@@ -20,6 +20,27 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
+export const Note = sequelize.define('notes', {
+  note: Sequelize.TEXT,
+  tag: Sequelize.STRING
+});
+
+sequelize.sync({ force: true }).then(() => {
+  console.log(`Database & tables created!`);
+
+  Note.bulkCreate([
+    { note: 'pick up some bread after work', tag: 'shopping' },
+    { note: 'remember to write up meeting notes', tag: 'work' },
+    { note: 'learn how to use node orm', tag: 'work' }
+  ])
+    .then(function () {
+      return Note.findAll();
+    })
+    .then(function (notes) {
+      console.log(notes);
+    });
+});
+
 /*
  *  Setting up the Express app
  */
